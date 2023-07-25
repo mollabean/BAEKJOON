afdad7f6b5a3464a7cdd77dbfa3ac712643edd41
mollabean <113323674+mollabean@users.noreply.github.com>
2023-07-25 22:25:04 +0900

    [Bronze V] Title: A+B - 4, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        #a, b = map(int, input().split())
+        a, b = input().split()
+        a = int(a)
+        b = int(b)
+        print(a+b)
+    except:
+        break
+        
