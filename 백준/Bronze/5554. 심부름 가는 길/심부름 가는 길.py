fca7c00f98a0cd402abcee6c3f374134b03e0f30
mollabean <113323674+mollabean@users.noreply.github.com>
2023-09-16 19:10:52 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.py
@@ -0,0 +1,8 @@
+import sys
+first = int(sys.stdin.readline())
+second = int(sys.stdin.readline())
+third = int(sys.stdin.readline())
+fourth = int(sys.stdin.readline())
+
+print((first+second+third+fourth) // 60)
+print((first+second+third+fourth) % 60)
